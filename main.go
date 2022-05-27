79e2488eb2eb696fac3923e53b3ad27fabdf6c21
Vinicius Oliveira <runnice@gmail.com>
2022-05-27 16:29:38 -0300

    criando server

---
+++ main.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"io"
+	"log"
+	"os"
+
+	"github.com/gin-gonic/gin"
+)
+
+func helloHandler(c *gin.Context) {
+	c.JSON(200, gin.H{
+		"message": "Hello World!",
+	})
+}
+
+func main() {
+	// Criando router com Gin
+	router := gin.Default()
+
+	// gin.SetMode("release") // Opção para o Gin em produção
+
+	// Criando arquivo de log do servidor
+	f, err := os.Create("gin.log")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	gin.DefaultWriter = io.MultiWriter(f)
+	// Capturar o request GET "/hello-world"
+	router.GET("/hello-world", helloHandler)
+	router.Run()
+
+}
